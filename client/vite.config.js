b80eba453976202beb4a4c7b97215851882454be
Prem Sharma <sonusharma4237@gmail.com>
2025-09-07 10:41:17 +0530

    deploye fd phase 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(),tailwindcss()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://company-analyzer-tyeq.vercel.app/', // <--- Update this line
         changeOrigin: true,
         secure: false,
       }
